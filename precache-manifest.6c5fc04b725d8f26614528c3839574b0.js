1688cce4c6dc2891cf8442457ccc3ac0b1360c03
manhddbhsoft <manhdd.bhsoft@gmail.com>
2020-06-26 00:17:45 +0700

    Updates

---
+++ precache-manifest.6c5fc04b725d8f26614528c3839574b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8a03cab00951a4b644d6fc30d1bac3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf3f2e445a85557ba816",
+    "url": "/portfolio/static/css/main.55cb7ad1.chunk.css"
+  },
+  {
+    "revision": "a3c87166ab4ebb7c4d5c",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3f2e445a85557ba816",
+    "url": "/portfolio/static/js/main.d2437b6b.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  }
+]);
